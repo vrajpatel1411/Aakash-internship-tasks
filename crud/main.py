c4f9e6d616d451f845324e81feb215f1c28f6036
Vraj Patel <56471759+vrajpatel1411@users.noreply.github.com>
2021-06-03 22:49:42 +0530

    Update main.py
    New check_balance and Deposit_money method is added

--- crud/main.py
+++ crud/main.py
@@ -11,18 +11,17 @@ def password():
                      'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
                      'Z']
  
-    SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
-           '*', '(', ')', '<']
+    
 
-    COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+    COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS 
  
 
     rand_digit = random.choice(DIGITS)
     rand_upper = random.choice(UPCASE_CHARACTERS)
     rand_lower = random.choice(LOCASE_CHARACTERS)
-    rand_symbol = random.choice(SYMBOLS)
+
  
-    temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
+    temp_pass = rand_digit + rand_upper + rand_lower
  
  
     for x in range(MAX_LEN - 4):
@@ -57,7 +56,7 @@ def create(option,mydb):
     mydb.commit()
     user_password=password()
 
-    banking_sql="insert into banking_details (user_id,user_name,mobile_number,login_id,password) values(%s,%s,%s,%s,%s)"
+    banking_sql="insert into banking1(user_id,user_name,mobile_number,login_id,password) values(%s,%s,%s,%s,%s)"
    
     banking_var=(user_id,name,mobile_number,login_id,user_password)
     mycursor.execute(banking_sql,banking_var)
@@ -71,7 +70,7 @@ def create(option,mydb):
     mydb.commit()
     
     print("Your account is created successfully")
-    sql="SELECT user_id,user_name,login_id,password FROM banking_details where user_id={}".format(user_id)
+    sql="SELECT user_id,user_name,login_id,password FROM banking1 where user_id={}".format(user_id)
 
     
     mycursor.execute(sql)
@@ -84,6 +83,46 @@ def create(option,mydb):
 
 
 
+def check_balance(mydb):
+    
+    print("Welcome to Vraj Patel Bank")
+    login_id=input("Enter your login id=>")
+    password=input("Enter your password=>")
+    mycursor=mydb.cursor()
+    sql="select balance from balance_details inner join banking1 on balance_details.login_id=banking1.login_id where balance_details.login_id=%s and banking1.password=%s" 
+    mycursor.execute(sql,(login_id,password,))
+    myresult = mycursor.fetchone()
+    if myresult is None:
+        print("you have enter wrong Id and Password")
+    else:
+        for x in myresult:
+            print(x) 
+ 
+def Deposit_money(mydb):
+    print("Welcome to Vraj Patel Bank")
+    login_id=input("Enter your login id=>")
+    password=input("Enter your password=>")
+    mycursor=mydb.cursor()
+    sql="select balance from balance_details inner join banking1 on balance_details.login_id=banking1.login_id where balance_details.login_id=%s and banking1.password=%s" 
+    mycursor.execute(sql,(login_id,password,))
+    myresult = mycursor.fetchone()
+    if myresult is None:
+        print("you have enter wrong Id and Password")
+    else:
+        amount=int(input("Enter the amount you want to deposit=>"))
+        sql="select balance from balance_details inner join banking1 on balance_details.login_id=banking1.login_id where balance_details.login_id=%s and banking1.password=%s" 
+        mycursor.execute(sql,(login_id,password,))
+        myresult = mycursor.fetchone()
+        for x in myresult:
+            amount=amount+x     
+        sql="update balance_details set balance=%s where login_id=%s"
+        var=(amount,login_id)
+        mycursor.execute(sql,var)
+        mydb.commit()
+        print(mycursor.rowcount, "record(s) affected")
+
+
+
 
 
 
@@ -92,8 +131,9 @@ def create(option,mydb):
 import mysql.connector
 import random
 import array
+
 mydb=mysql.connector.connect(
-    host="localhost",
+    host="127.0.0.1",
     user="root",
     password="vraj1411",
     database="Banking"
@@ -112,3 +152,10 @@ option=int(input())
 
 if option==1:
     create(option,mydb)
+
+elif option==2:
+    check_balance(mydb)
+    
+elif option==3:
+    Deposit_money(mydb)
+    
